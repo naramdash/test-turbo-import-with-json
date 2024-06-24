90214c295753a9532e5493dbd415cc9dd398278f
naramdash <juho_kim@outlook.com>
2024-06-24 18:35:35 +0900

    init

---
+++ app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { SomeData } from '@monorepo/package-with-json'
+</script>
+
+<template>
+  <div>
+    {{ SomeData }}
+  </div>
+</template>
---
+++ package-with-json/src/index.ts
@@ -0,0 +1,3 @@
+import SomeData from './data/SomeData.json' with {type: 'json'}
+
+export {SomeData}
